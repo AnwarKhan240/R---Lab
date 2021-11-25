f0664528489a31a27ebb6705e9ad4b364553a6aa
Anwar Khan <94431998+AnwarKhan240@users.noreply.github.com>
2021-11-25 11:00:22 +0530

    Add files via upload

---
+++ bigof2.R
@@ -0,0 +1,5 @@
+a = as.integer(readline(prompt = "Enter 1st Number: "))
+b = as.integer(readline(prompt = "Enter 2nd Number: "))
+if(a>b)
+print("A is Big")else
+print("B is Big")
---
+++ vote.R
@@ -0,0 +1,4 @@
+a = as.integer(readline(prompt = "Enter your age: "))
+if(a >= 18)
+print("Eligible to vote")else
+print("Not Eligible to vote")
