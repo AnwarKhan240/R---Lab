556699cec1494906ce07b6723039ae55f73a97ca
Anwar Khan <94431998+AnwarKhan240@users.noreply.github.com>
2021-11-25 11:06:16 +0530

    Add files via upload

---
+++ bigof3.R
@@ -0,0 +1,7 @@
+a = as.integer(readline(prompt = "Enter 1st value: "))
+b = as.integer(readline(prompt = "Enter 2nd value: "))
+c = as.integer(readline(prompt = "Enter 3rd value: "))
+if(a>b && a>c)
+print("A is Big")else if(b>c)
+print("B is Big")else
+print("C is Big")
